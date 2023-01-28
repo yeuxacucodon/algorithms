b51ddc9c1fa5dd763ee28c90a262d147c64a3ae1
yeuxacucodon <chiendien2006@gmail.com>
2023-01-28 16:31:35 +0700

    update

---
+++ factorSum.cpp
@@ -0,0 +1,30 @@
+// https://codelearn.io/learning/thuat-toan-can-ban/3811
+#include <iostream>
+using namespace std;
+
+int solve(int n) {
+  int k = 2, sum = 0;
+  while (n > 1) {
+    while (n % k == 0) {
+      sum += k;
+      n /= k;
+    }
+    k++;
+  }
+  return sum;
+}
+
+int factorSum(int n) {
+  while (n != solve(n)) {
+    n = solve(n);
+  }
+  return n;
+}
+
+int main() {
+  cout << "n = ";
+  int n;
+  cin >> n;
+  int result = factorSum(n);
+  cout << result;
+}
