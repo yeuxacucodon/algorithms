39ce21eb02fa463ce3e9e9d80e7b24f3c5807ecb
yeuxacucodon <chiendien2006@gmail.com>
2023-01-28 21:39:34 +0700

    update

--- lastDigitDiffZero.cpp
+++ lastDigitDiffZero.cpp
@@ -3,16 +3,18 @@
 using namespace std;
 
 int lastDigitDiffZero(int n) {
-  long long res = 1;
+  long long result = 1;
   for (int i = 2; i <= n; i++) {
-    res *= i;
-    while (res % 10 == 0)
-      res /= 10;
-    res = res % 100;
+    result *= i;
+    while (result % 10 == 0) {
+      result /= 10;
+    }
+    result = result % 100;
   }
-  while (res % 10 == 0)
-    res /= 10;
-  return res % 10;
+  while (result % 10 == 0) {
+    result /= 10;
+  }
+  return result % 10;
 }
 
 int main() {
