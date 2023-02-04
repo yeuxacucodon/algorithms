0accb8933c579c7690849e1f82f1ac4a5af76ff3
yeuxacucodon <chiendien2006@gmail.com>
2023-02-04 20:55:40 +0700

    update

---
+++ linearSearch.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int linearSearch(int arr[], int n, int x) {
+  for (int i = 0; i < n; i++) {
+    if (arr[i] == x) {
+      return i;
+    }
+  }
+  return -1;
+}
+
+int main() {
+  int n, arr[1000], target;
+  cin >> n;
+  cin >> target;
+  for (int i = 0; i < n; i++) {
+    cin >> arr[i];
+  }
+  int result = linearSearch(arr, n, target);
+  if (result == -1) {
+    cout << "Can't find " << target;
+  } else {
+    cout << "Position is "
+         << "arr[" << result << "]";
+  }
+  return 0;
+}
