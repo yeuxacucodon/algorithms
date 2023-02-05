208409101a8efd93c762664cdbb323994f1956d5
yeuxacucodon <chiendien2006@gmail.com>
2023-02-05 19:46:36 +0700

    update

--- insertionsort.cpp
+++ insertionsort.cpp
@@ -2,15 +2,15 @@
 using namespace std;
 
 void sort(int arr[], int n) {
-  int key, j;
+  int temp, j;
   for (int i = 1; i < n; i++) {
-    key = arr[i];
+    temp = arr[i];
     j = i - 1;
-    while (j >= 0 && arr[j] > key) {
+    while (j >= 0 && arr[j] > temp) {
       arr[j + 1] = arr[j];
       j--;
     }
-    arr[j + 1] = key;
+    arr[j + 1] = temp;
   }
 }
 
