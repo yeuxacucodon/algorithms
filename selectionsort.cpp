b5ad2fb58257d4faaab01b43699b6127d08fe59c
Vo Quang Chien <chiendien2006@gmail.com>
2023-02-23 14:27:49 +0700

    add selectionsort

---
+++ selectionsort.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+void selectionsort(int arr[], int n) {
+  for (int i = 0; i < n - 1; i++) {
+    int min = i;
+    for (int j = i + 1; j < n; j++) {
+      if (arr[j] < arr[min]) {
+        min = j;
+      }
+    }
+    swap(arr[min], arr[i]);
+  }
+}
+
+int main() {
+  int n, arr[1000];
+  cout << "n = ";
+  cin >> n;
+  for (int i = 0; i < n; i++) {
+    cout << "arr[" << i << "] = ";
+    cin >> arr[i];
+  }
+  selectionsort(arr, n);
+  for (int i = 0; i < n; i++) {
+    cout << arr[i] << " ";
+  }
+}
